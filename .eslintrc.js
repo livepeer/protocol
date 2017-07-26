5679a8964a8fd093695facd904a9b6dd5327d6ec
Leo Shklovskii <leo@thermopylae.net>
2017-07-26 10:45:28 -0400

    eslint config and vscode task

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": ["google"],
+	"parserOptions": {
+		"ecmaVersion": 8,
+		"sourceType": "module"
+	},
+	"rules": {
+		"arrow-parens": ["error", "as-needed"],
+		"max-len": "off",
+		"semi": "off",
+		"quotes": ["error", "double"],
+	}
+};
