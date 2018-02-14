a634bbedd3689a4b623b3856ef33ac541c4ab44f
Yondon Fu <yondon.fu@gmail.com>
2018-02-14 12:47:59 -0500

    More descriptive comments for proxy contracts

--- contracts/ManagerProxyTarget.sol
+++ contracts/ManagerProxyTarget.sol
@@ -3,6 +3,13 @@ pragma solidity ^0.4.17;
 import "./Manager.sol";
 
 
+/**
+ * @title ManagerProxyTarget
+ * @dev The base contract that target contracts used by a proxy contract should inherit from
+ * Note: Both the target contract and the proxy contract (implemented as ManagerProxy) MUST inherit from ManagerProxyTarget in order to guarantee
+ * that both contracts have the same storage layout. Differing storage layouts in a proxy contract and target contract can
+ * potentially break the delegate proxy upgradeability mechanism
+ */
 contract ManagerProxyTarget is Manager {
     // Used to look up target contract address in controller's registry
     bytes32 public targetContractId;
