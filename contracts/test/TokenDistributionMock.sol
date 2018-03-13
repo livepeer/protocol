ece20be712502becbc5dffef13502c1989259299
Yondon Fu <yondon.fu@gmail.com>
2018-03-13 19:28:30 -0400

    Use SafeMath in dummy token distribution

--- contracts/test/TokenDistributionMock.sol
+++ contracts/test/TokenDistributionMock.sol
@@ -1,16 +1,19 @@
 pragma solidity ^0.4.17;
 
 import "../token/ITokenDistribution.sol";
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
 
 
 contract TokenDistributionMock is ITokenDistribution {
+    using SafeMath for uint256;
+
     // End time of the distribution
     uint256 endTime;
     // Is the distribution over
     bool over;
 
     function TokenDistributionMock(uint256 _timeToEnd) public {
-        endTime = now + _timeToEnd;
+        endTime = now.add(_timeToEnd);
         over = false;
     }
 
