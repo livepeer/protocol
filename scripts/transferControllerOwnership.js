59040933aa1406c0bde94c1552bf6d24d5eb8050
Yondon Fu <yondon.fu@gmail.com>
2018-03-23 16:03:34 -0400

    Additional logging for ownership transfer script

--- scripts/transferControllerOwnership.js
+++ scripts/transferControllerOwnership.js
@@ -3,5 +3,11 @@ const genesisConfig = require("../migrations/genesis.config.js")
 
 module.exports = async () => {
     const controller = await Controller.deployed()
+
+    console.log(`Transferring ownership of the Controller at ${controller.address} to the governance multisig at ${genesisConfig.governanceMultisig}...`)
+
     await controller.transferOwnership(genesisConfig.governanceMultisig)
+
+    const newOwner = await controller.owner()
+    console.log(`Controller at ${controller.address} is now owned by ${newOwner}`)
 }
