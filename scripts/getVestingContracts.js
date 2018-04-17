88adeb29b3903659b852e5abbb051b8d04432f5d
Eric Tang <eric.x.tang@gmail.com>
2018-04-17 17:21:50 -0400

    always load the newly deployed controller

--- scripts/getVestingContracts.js
+++ scripts/getVestingContracts.js
@@ -2,7 +2,7 @@ const GenesisManager = artifacts.require("GenesisManager")
 const genesisConfig = require("../migrations/genesis.config.js")
 
 module.exports = async () => {
-    const genesisManager = await GenesisManager.at("0x9a4d4cafbb1fc3b00d387095144e34b993d82d9a")
+    const genesisManager = await GenesisManager.deployed()
 
     genesisConfig.teamGrants.forEach(async grant => {
         const vestingHolderAddr = await genesisManager.vestingHolders.call(grant.receiver)
