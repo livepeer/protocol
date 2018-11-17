cebcbdc6eb7dd7398570c1c04aea11839439c140
Yondon Fu <yondon.fu@gmail.com>
2018-11-17 14:30:53 -0500

    Update CircleCI

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,5 @@
 const glob = require("glob")
 
-const previousFiles = glob.sync("contracts/previous/**/*.sol").map(file => file.replace("contracts/", ""))
 const testFiles = glob.sync("contracts/test/*.sol").map(file => file.replace("contracts/", ""))
 const mockFiles = ["rounds/AdjustableRoundsManager.sol"]
 const interfaces = [
@@ -14,12 +13,11 @@ const interfaces = [
     "verification/IVerifiable.sol",
     "verification/IVerifier.sol"
 ]
-const miscFiles = ["token/MerkleMine.sol"] // Tests are here https://github.com/livepeer/merkle-mine/blob/master/test/MerkleMine.js so skip for this repo
 
 module.exports = {
     norpc: true,
     testCommand: "node --max-old-space-size=4096 ../node_modules/.bin/truffle test test/unit/* --network coverage",
     compileCommand: "node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage",
-    copyPackages: ["zeppelin-solidity"],
-    skipFiles: previousFiles.concat(testFiles).concat(mockFiles).concat(interfaces).concat(miscFiles)
+    copyPackages: ["openzeppelin-solidity"],
+    skipFiles: testFiles.concat(mockFiles).concat(interfaces)
 }
