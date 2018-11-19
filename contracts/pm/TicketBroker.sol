0fa8588453a4828f3e23cca478c430e1f6dc0221
eladmallel <eladmallel@gmail.com>
2018-11-19 15:54:18 -0500

    PM: fund deposit and penalty escrow
    As discussed in the Minimal Viable Streamflow spec, we're starting off
    with a hard-coded min penalty escrow, with future plans to bound
    max financial activity before double spend is detected using more
    robust approaches.

--- contracts/pm/TicketBroker.sol
+++ contracts/pm/TicketBroker.sol
@@ -3,8 +3,33 @@ pragma solidity ^0.4.25;
 
 contract TicketBroker {
 
+    struct Sender {
+        uint256 deposit;
+        uint256 penaltyEscrow;
+    }
+
+    uint256 public minPenaltyEscrow;
+
+    mapping (address => Sender) public senders;
+
+    event DepositFunded(address indexed sender, uint256 amount);
+    event PenaltyEscrowFunded(address indexed sender, uint256 amount);
+
+    constructor(uint256 _minPenaltyEscrow) public {
+        minPenaltyEscrow = _minPenaltyEscrow;
+    }
+
     function fundDeposit() external payable {
-        // update sender's balance
-        // emit an event
+        senders[msg.sender].deposit += msg.value;
+
+        emit DepositFunded(msg.sender, msg.value);
+    }
+
+    function fundPenaltyEscrow() external payable {
+        require(msg.value >= minPenaltyEscrow, "tx value must be >= minPenaltyEscrow");
+
+        senders[msg.sender].penaltyEscrow += msg.value;
+
+        emit PenaltyEscrowFunded(msg.sender, msg.value);
     }
 }
