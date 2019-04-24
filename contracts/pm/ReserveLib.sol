f2ec594fa965f0fe71f0c72ce24e93c75d2473a4
Yondon Fu <yondon.fu@gmail.com>
2019-04-24 18:25:53 -0400

    pm: Support freezing and claiming in ReserveLib

--- contracts/pm/ReserveLib.sol
+++ contracts/pm/ReserveLib.sol
@@ -16,7 +16,7 @@ library ReserveLib {
     struct ReserveManager {
         uint256 reserveNonce;
         Reserve reserve;
-        mapping (uint256 => mapping (address => uint256)) claimed;
+        mapping (uint256 => mapping (address => uint256)) claimedPerReserve;
     }
 
     function fund(ReserveManager storage manager, uint256 _amount) internal {
@@ -29,8 +29,61 @@ library ReserveLib {
         manager.reserveNonce = manager.reserveNonce.add(1);
     }
 
+    function freeze(
+        ReserveManager storage manager,
+        uint256 _freezeRound,
+        uint256 _recipientsInFreezeRound
+    )
+        internal
+    {
+        Reserve storage reserve = manager.reserve;
+
+        reserve.freezeRound = _freezeRound;
+        reserve.recipientsInFreezeRound = _recipientsInFreezeRound;
+    }
+
+    function claim(
+        ReserveManager storage manager,
+        address _claimant,
+        uint256 _amount
+    )
+        internal
+        returns (uint256)
+    {
+        Reserve storage reserve = manager.reserve;
+
+        if (reserve.freezeRound == 0 || reserve.recipientsInFreezeRound == 0) {
+            return 0;
+        }
+
+        uint256 reserveID = manager.reserveNonce;
+        uint256 allocation = reserve.fundsAdded.div(reserve.recipientsInFreezeRound);
+        uint256 claimedFunds = manager.claimedPerReserve[reserveID][_claimant];
+        uint256 claimableFunds = allocation.sub(claimedFunds);
+
+        if (_amount > claimableFunds) {
+            manager.claimedPerReserve[reserveID][_claimant] = allocation;
+            reserve.fundsClaimed = allocation;
+
+            return claimableFunds;
+        } else {
+            manager.claimedPerReserve[reserveID][_claimant] = claimedFunds.add(_amount);
+            reserve.fundsClaimed = reserve.fundsClaimed.add(_amount);
+
+            return _amount;
+        }
+    }
+
+    function claimed(ReserveManager storage manager, address _claimant) internal view returns (uint256) {
+        return manager.claimedPerReserve[manager.reserveNonce][_claimant];
+    }
+
     function fundsRemaining(ReserveManager storage manager) internal view returns (uint256) {
         Reserve storage reserve = manager.reserve;
         return reserve.fundsAdded.sub(reserve.fundsClaimed);
+    }
+
+    function isFrozen(ReserveManager storage manager) internal view returns (bool) {
+        return manager.reserve.freezeRound > 0;
     }
 }
--- contracts/test/public/PublicReserveLib.sol
+++ contracts/test/public/PublicReserveLib.sol
@@ -10,6 +10,8 @@ contract PublicReserveLib {
 
     ReserveLib.ReserveManager internal manager;
 
+    event Claimed(address claimant, uint256 amount);
+
     function fund(uint256 _amount) external {
         manager.fund(_amount);
     }
@@ -18,8 +20,28 @@ contract PublicReserveLib {
         manager.clear();
     }
 
+    function freeze(uint256 _freezeRound, uint256 _recipientsInFreezeRound) external {
+        manager.freeze(_freezeRound, _recipientsInFreezeRound);
+    }
+
+    function claim(address _claimant, uint256 _amount) external {
+        uint256 res = manager.claim(_claimant, _amount);
+
+        // Emit an event with the result from manager.claim()
+        // for testing purposes
+        emit Claimed(_claimant, res);
+    }
+
+    function claimed(address _claimant) public view returns (uint256) {
+        return manager.claimed(_claimant);
+    }
+
     function fundsRemaining() public view returns (uint256) {
         return manager.fundsRemaining();
+    }
+
+    function isFrozen() public view returns (bool) {
+        return manager.isFrozen();
     }
 
     function getReserveNonce() public view returns (uint256) {
