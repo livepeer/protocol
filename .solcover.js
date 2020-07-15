6842713ac590070ab2c6a3b5d86cc288ee8734c4
Nico Vergauwen <vergauwennico@gmail.com>
2020-07-15 18:57:45 +0200

    fix CI

--- .solcover.js
+++ .solcover.js
@@ -2,7 +2,7 @@ module.exports = {
     client: require("ganache-cli"),
     providerOptions: {
         hardfork: "istanbul",
-        gasLimit: "0x7A1200",
+        gasLimit: "0xfffffffffff",
         total_accounts: 310
     },
     skipFiles: ["Migrations.sol", "test", "zeppelin", "rounds/AdjustableRoundsManager.sol", "pm/mixins/interfaces"]
