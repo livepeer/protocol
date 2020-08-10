a75e2b847031307f28f65ef1400a8f9a3cb6ff28
Nico Vergauwen <nico@livepeer.org>
2020-08-10 18:04:33 +0200

    fix SortedDoublyLL link when using unitTest network (#389)

--- migrations/2_deploy_libraries.js
+++ migrations/2_deploy_libraries.js
@@ -2,10 +2,7 @@ const SortedDoublyLL = artifacts.require("SortedDoublyLL")
 const BondingManager = artifacts.require("BondingManager")
 
 module.exports = function(deployer, network) {
-    if (network === "unitTest") {
-        return
-    }
-
+    if (network  === "unitTest") return
     deployer.deploy(SortedDoublyLL)
     deployer.link(SortedDoublyLL, BondingManager)
 }
