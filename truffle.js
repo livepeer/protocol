6f75e5573515c1281642b4871c321328f0606795
Nico Vergauwen <vergauwennico@gmail.com>
2020-09-03 16:53:12 +0200

    bump ganache gas limit to 10M

--- truffle.js
+++ truffle.js
@@ -56,7 +56,7 @@ module.exports = {
             host: "localhost",
             port: 8545,
             network_id: "*", // Match any network id
-            gas: 8000000
+            gas: 10000000
         },
         // This network should be used when running unit tests so migrations can be skipped
         unitTest: {
