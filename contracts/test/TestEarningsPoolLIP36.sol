1134c64b37ddbf1447fd252f6debdfe5a193b424
Nico Vergauwen <vergauwennico@gmail.com>
2020-09-10 13:11:34 +0200

    bonding: LIP36 EarningsPool

---
+++ contracts/test/TestEarningsPoolLIP36.sol
@@ -0,0 +1,69 @@
+pragma solidity ^0.5.11;
+
+import "./mocks/EarningsPoolFixture.sol";
+import "./helpers/truffle/Assert.sol";
+import "../libraries/MathUtils.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+
+contract TestEarningsPoolLIP36 {
+    using SafeMath for uint256;
+
+    EarningsPoolFixture fixture;
+
+    function beforeEach() public {
+        fixture = new EarningsPoolFixture();
+        fixture.setStake(1000);
+        fixture.setCommission(500000, 500000);
+    }
+
+    function test_updateCumulativeFeeFactor_no_prevEarningsPool() public {
+        uint256 fees = 1000;
+        
+        // earningsPool.cumulativeFeeFactor == 0
+        // prevEarningsPool.cumulativeFeeFactor == 0 
+        // prevEarningsPool.cumulativeRewardFactor == 0 
+        fixture.updateCumulativeFeeFactor(fees);
+        uint256 expFeeFactor = MathUtils.percPoints(fees, fixture.getTotalStake());
+        Assert.equal(fixture.getCumulativeFeeFactor(), expFeeFactor, "should set cumulativeFeeFactor");
+        
+        // earningsPool.cumulativeFeeFactor != 0 
+        fixture.updateCumulativeFeeFactor(fees);
+        expFeeFactor = expFeeFactor.add(MathUtils.percPoints(fees, fixture.getTotalStake()));
+        Assert.equal(fixture.getCumulativeFeeFactor(), expFeeFactor, "should update cumulativeFeeFactor");
+    }
+
+    function test_updateCumulativeFeeFactor_prevEarningsPool() public {
+        uint256 fees = 200;
+
+        // prevEarningsPool.cumulativeFeeFactor = 2
+        // prevEarningsPool.cumulativeRewardFactor = 3
+        uint256 prevFeeFactor = 2;
+        uint256 prevRewFactor = 3;
+        fixture.setPrevPoolEarningsFactors(prevFeeFactor, prevRewFactor);
+
+        // earningsPool.cumulativeFeeFactor == 0
+        fixture.updateCumulativeFeeFactor(fees);
+        uint256 expFeeFactor = prevFeeFactor.add(MathUtils.percOf(prevRewFactor, fees, fixture.getTotalStake()));
+        Assert.equal(fixture.getCumulativeFeeFactor(), expFeeFactor, "should update cumulativeFeeFactor");
+
+        // earningsPool.cumulativeFeeFactor != 0 
+        fixture.updateCumulativeFeeFactor(fees);
+        expFeeFactor = expFeeFactor.add(MathUtils.percOf(prevRewFactor, fees, fixture.getTotalStake()));
+    }
+
+    function test_updateCumulativeRewardFactor() public {
+        uint256 rewards = 1000;
+
+        // prevEarningsPool.cumulativeRewardFactor == 0
+        uint256 expRewardFactor = MathUtils.percPoints(1,1).add(MathUtils.percOf(MathUtils.percPoints(1,1), rewards, fixture.getTotalStake()));
+        fixture.updateCumulativeRewardFactor(1000);
+        Assert.equal(expRewardFactor, fixture.getCumulativeRewardFactor(), "incorrect cumulative reward factor");
+   
+        // prevEarningsPool.cumulativeRewardFactor != 0
+        fixture.setPrevPoolEarningsFactors(0, expRewardFactor);
+        expRewardFactor = expRewardFactor.add(MathUtils.percOf(expRewardFactor, rewards, fixture.getTotalStake()));
+        fixture.updateCumulativeRewardFactor(1000);
+        Assert.equal(expRewardFactor, fixture.getCumulativeRewardFactor(), "incorrect cumulative reward factor");
+    }
+}
--- test/unit/EarningsPool.js
+++ test/unit/EarningsPool.js
@@ -3,3 +3,4 @@ import runSolidityTest from "./helpers/runSolidityTest"
 runSolidityTest("TestEarningsPool")
 runSolidityTest("TestEarningsPool2")
 runSolidityTest("TestEarningsPoolNoTranscoderRewardFeePool")
+runSolidityTest("TestEarningsPoolLIP36")
