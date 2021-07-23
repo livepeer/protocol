bbf81c24248d35b36afe996917e90d1d0175ed36
Kautuk Kundan <kautukkundan@gmail.com>
2021-07-23 22:25:45 +0530

    Added typescript (#420)
    * typescript: added typescript support with hardhat
    * scripts: added running scripts for hardhat
    * plugins: added hardhat plugins
    * typescript: typebindings for hardhat user config
    * typescript: added hardhat config to babel ignore

---
+++ hardhat.config.ts
@@ -0,0 +1,32 @@
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-web3";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "@nomiclabs/hardhat-etherscan";
+import { HardhatUserConfig } from "hardhat/types/config";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.5.11",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      blockGasLimit: 12000000,
+    },
+  },
+  gasReporter: {
+    enabled: true,
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
