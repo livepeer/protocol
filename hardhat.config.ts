8e7ce77fd9ab679b75eacd04213c27555b27ffa1
Nico Vergauwen <vergauwennico@gmail.com>
2021-07-28 18:22:59 +0200

    bump hardhat version and add networks to config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,6 +11,9 @@ import 'hardhat-deploy-ethers';
 
 import { HardhatUserConfig } from "hardhat/types/config";
 
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const INFURA_KEY = process.env.INFURA_KEY
+
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.5.11",
@@ -24,13 +27,28 @@ const config: HardhatUserConfig = {
   namedAccounts: {
     deployer: 0,
   },
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
+      gas: 12000000,
+      allowUnlimitedContractSize: true,
       blockGasLimit: 12000000,
     },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
+      accounts: PRIVATE_KEY ? [`0x${PRIVATE_KEY}`] : undefined
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
+      accounts: PRIVATE_KEY ? [`0x${PRIVATE_KEY}`] : undefined,
+      blockGasLimit: 12000000,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    }
   },
   gasReporter: {
-    enabled: true,
+    enabled: (process.env.REPORT_GAS) ? true : false
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
