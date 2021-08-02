11a612be1d9bf3876e370a726b5a43779033241a
Nico Vergauwen <vergauwennico@gmail.com>
2021-08-03 01:30:06 +0200

    remove truffle migrations

--- migrations/1_initial_migration.js
+++
@@ -1,9 +0,0 @@
-const Migrations = artifacts.require("./Migrations.sol")
-
-module.exports = function(deployer, network) {
-    if (network === "unitTest") {
-        return
-    }
-
-    deployer.deploy(Migrations)
-}
