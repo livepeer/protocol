94406c8b8c88923416ba42264b934c6dc8ef741b
Nico Vergauwen <vergauwennico@gmail.com>
2021-08-17 16:46:08 +0200

    Controller: update compiler version

--- contracts/IController.sol
+++ contracts/IController.sol
@@ -1,17 +1,21 @@
-pragma solidity ^0.5.11;
+// SPDX-FileCopyrightText: 2021 Livepeer <info@livepeer.org>
 
-import "./zeppelin/Pausable.sol";
+// SPDX-License-Identifier: GPL-3.0
 
-contract IController is Pausable {
+pragma solidity 0.8.4;
+
+import "./utils/Pausable.sol";
+
+abstract contract IController is Pausable {
     event SetContractInfo(bytes32 id, address contractAddress, bytes20 gitCommitHash);
 
     function setContractInfo(
         bytes32 _id,
         address _contractAddress,
         bytes20 _gitCommitHash
-    ) external;
+    ) external virtual;
 
-    function updateController(bytes32 _id, address _controller) external;
+    function updateController(bytes32 _id, address _controller) external virtual;
 
-    function getContract(bytes32 _id) public view returns (address);
+    function getContract(bytes32 _id) external view virtual returns (address);
 }
