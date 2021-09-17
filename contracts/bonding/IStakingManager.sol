1e95054a2eee93b8891751508b476a8333fadf82
kautuk kundan <kautukkundan@gmail.com>
2021-09-18 04:05:55 +0530

    stakingManager: updated API separating orcherstrators and delegators

--- contracts/bonding/IStakingManager.sol
+++ contracts/bonding/IStakingManager.sol
@@ -4,47 +4,32 @@
 pragma solidity 0.8.4;
 
 /**
- * @title Interface for BondingManager
+ * @title Interface for StakingManager
  */
 interface IStakingManager {
-    event TranscoderUpdate(address indexed transcoder, uint256 rewardCut, uint256 feeShare);
-    event TranscoderActivated(address indexed transcoder, uint256 activationRound);
-    event TranscoderDeactivated(address indexed transcoder, uint256 deactivationRound);
-    event TranscoderSlashed(address indexed transcoder, address finder, uint256 penalty, uint256 finderReward);
-    event Reward(address indexed transcoder, uint256 amount);
-    event Bond(address indexed delegate, address indexed delegator, uint256 additionalAmount, uint256 bondedAmount);
-    event Unbond(
-        address indexed delegate,
-        address indexed delegator,
-        uint256 unbondingLockId,
-        uint256 amount,
-        uint256 withdrawRound
-    );
-    event Rebond(address indexed delegate, address indexed delegator, uint256 unbondingLockId, uint256 amount);
+    event OrchestratorUpdate(address indexed orchestrator, uint256 rewardCut, uint256 feeShare);
+    event OrchestratorActivated(address indexed orchestrator, uint256 activationRound);
+    event OrchestratorDeactivated(address indexed orchestrator, uint256 deactivationRound);
+    event Reward(address indexed orchestrator, uint256 amount);
+
+    event Stake(address indexed orchestrator, uint256 amount);
+    event Unstake(address indexed orchestrator, uint256 amount, uint256 lockID);
+
+    event Delegate(address indexed delegator, address indexed orchestrator, uint256 amount);
+    event Undelegate(address indexed delegator, address indexed orchestrator, uint256 amount, uint256 lockID);
+
     event WithdrawStake(address indexed delegator, uint256 unbondingLockId, uint256 amount, uint256 withdrawRound);
     event WithdrawFees(address indexed delegator);
-    event EarningsClaimed(
-        address indexed delegate,
-        address indexed delegator,
-        uint256 rewards,
-        uint256 fees,
-        uint256 startRound,
-        uint256 endRound
-    );
 
     // External functions
-    function updateTranscoderWithFees(
-        address _transcoder,
-        uint256 _fees,
-        uint256 _round
-    ) external;
+    function updateOrchestratorWithFees(address _orchestrator, uint256 _fees) external;
 
     function setCurrentRoundTotalActiveStake() external;
 
     // Public functions
-    function getTranscoderPoolSize() external view returns (uint256);
+    function getOrchestratorPoolSize() external view returns (uint256);
 
-    function isActiveTranscoder(address _transcoder) external view returns (bool);
+    function isActiveOrchestrator(address _orchestrator) external view returns (bool);
 
-    function getTotalBonded() external view returns (uint256);
+    function getTotalStaked() external view returns (uint256);
 }
