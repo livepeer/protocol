e01d59d127c924f51ae344b2eef3113fe05cb20d
Yondon Fu <yondon.fu@gmail.com>
2022-01-06 09:55:18 -0500

    test: Fix Solidity test runner

--- test/unit/MathUtils.js
+++ test/unit/MathUtils.js
@@ -1,7 +1,3 @@
 import runSolidityTest from "./helpers/runSolidityTest"
 
-describe("MathUtils", () => {
-    it("Runs solidity tests", async () => {
-        await runSolidityTest("TestMathUtils", ["AssertBool", "AssertUint"])
-    })
-})
+runSolidityTest("TestMathUtils", ["AssertBool", "AssertUint"])
--- test/unit/MathUtilsV2.js
+++ test/unit/MathUtilsV2.js
@@ -1,7 +1,3 @@
 import runSolidityTest from "./helpers/runSolidityTest"
 
-describe("MathUtilsV2", () => {
-    it("Runs solidity tests", async () => {
-        await runSolidityTest("TestMathUtilsV2", ["AssertBool", "AssertUint"])
-    })
-})
+runSolidityTest("TestMathUtilsV2", ["AssertBool", "AssertUint"])
