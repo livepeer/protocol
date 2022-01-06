00bf1a1cd4c123841026aa0d2ca0261cb41e002a
Yondon Fu <yondon.fu@gmail.com>
2022-01-06 09:55:18 -0500

    test: Fix Solidity test runner

--- test/unit/SortedDoublyLL.js
+++ test/unit/SortedDoublyLL.js
@@ -1,34 +1,30 @@
 import runSolidityTest from "./helpers/runSolidityTest"
 
-describe("SortedDoublyLL", () => {
-    it("Runs solidity tests", async () => {
-        await runSolidityTest("TestSortedDoublyLLFindWithHints", [
-            "SortedDoublyLL",
-            "AssertAddress",
-            "AssertUint"
-        ])
-        await runSolidityTest("TestSortedDoublyLLFindWithHints2", [
-            "SortedDoublyLL",
-            "AssertAddress",
-            "AssertUint"
-        ])
-        await runSolidityTest("TestSortedDoublyLLInsert", [
-            "SortedDoublyLL",
-            "AssertAddress",
-            "AssertUint",
-            "AssertBool"
-        ])
-        await runSolidityTest("TestSortedDoublyLLRemove", [
-            "SortedDoublyLL",
-            "AssertAddress",
-            "AssertUint",
-            "AssertBool"
-        ])
-        await runSolidityTest("TestSortedDoublyLLUpdateKey", [
-            "SortedDoublyLL",
-            "AssertAddress",
-            "AssertUint",
-            "AssertBool"
-        ])
-    })
-})
+runSolidityTest("TestSortedDoublyLLFindWithHints", [
+    "SortedDoublyLL",
+    "AssertAddress",
+    "AssertUint"
+])
+runSolidityTest("TestSortedDoublyLLFindWithHints2", [
+    "SortedDoublyLL",
+    "AssertAddress",
+    "AssertUint"
+])
+runSolidityTest("TestSortedDoublyLLInsert", [
+    "SortedDoublyLL",
+    "AssertAddress",
+    "AssertUint",
+    "AssertBool"
+])
+runSolidityTest("TestSortedDoublyLLRemove", [
+    "SortedDoublyLL",
+    "AssertAddress",
+    "AssertUint",
+    "AssertBool"
+])
+runSolidityTest("TestSortedDoublyLLUpdateKey", [
+    "SortedDoublyLL",
+    "AssertAddress",
+    "AssertUint",
+    "AssertBool"
+])
