a9d65e5760b1e3c2c0caaa9bc13b3a6538a916ba
Yondon Fu <yondon.fu@gmail.com>
2022-01-07 08:39:51 -0500

    polling: Use min stake in PollCreator

--- test/unit/PollCreator.js
+++ test/unit/PollCreator.js
@@ -1,10 +1,12 @@
 import Fixture from "./helpers/Fixture"
-import {functionSig} from "../../utils/helpers"
 import {web3, ethers} from "hardhat"
 
-import chai, {expect, assert} from "chai"
+import {expect, use} from "chai"
 import {solidity} from "ethereum-waffle"
-chai.use(solidity)
+import {smock} from "@defi-wonderland/smock"
+
+use(solidity)
+use(smock.matchers)
 
 const QUORUM = 333300
 const QUOTA = 500000
@@ -12,12 +14,22 @@ const POLL_PERIOD = 10 * 5760
 
 describe("PollCreator", () => {
     let fixture
-    let token
     let pollCreator
 
+    let bondingManagerMock
+    let mockBondingManagerEOA
+
     before(async () => {
+        ;[, mockBondingManagerEOA] = await ethers.getSigners()
+
         fixture = new Fixture(web3)
-        token = await (await ethers.getContractFactory("GenericMock")).deploy()
+
+        bondingManagerMock = await smock.fake(
+            "contracts/polling/PollCreator.sol:IBondingManager",
+            {
+                address: mockBondingManagerEOA.address
+            }
+        )
     })
 
     beforeEach(async () => {
@@ -32,11 +44,13 @@ describe("PollCreator", () => {
         before(async () => {
             pollCreator = await (
                 await ethers.getContractFactory("PollCreator")
-            ).deploy(token.address)
+            ).deploy(bondingManagerMock.address)
         })
 
-        it("initialize state: token", async () => {
-            assert.equal(await pollCreator.token(), token.address)
+        it("initialize state: bondingManager", async () => {
+            expect(await pollCreator.bondingManager()).to.be.equal(
+                bondingManagerMock.address
+            )
         })
     })
 
@@ -46,27 +60,97 @@ describe("PollCreator", () => {
         before(async () => {
             pollCreator = await (
                 await ethers.getContractFactory("PollCreator")
-            ).deploy(token.address)
+            ).deploy(bondingManagerMock.address)
         })
 
-        it("revert when not enough tokens approved", async () => {
+        it("revert when caller has insufficient stake", async () => {
+            const cost = await pollCreator.POLL_CREATION_COST()
+
+            // pendingStake() == 0 && transcoderTotalStake() == 0
             await expect(pollCreator.createPoll(hash)).to.be.revertedWith(
-                "LivepeerToken transferFrom failed"
+                "PollCreator#createPoll: INSUFFICIENT_STAKE"
+            )
+
+            // pendingStake() > 0 && transcoderTotalStake() == 0
+            bondingManagerMock.pendingStake.returns(cost.sub(1))
+            await expect(pollCreator.createPoll(hash)).to.be.revertedWith(
+                "PollCreator#createPoll: INSUFFICIENT_STAKE"
+            )
+
+            // pendingStake() == 0 && transcoderTotalStake() > 0
+            bondingManagerMock.pendingStake.returns(0)
+            bondingManagerMock.transcoderTotalStake.returns(cost.sub(1))
+            await expect(pollCreator.createPoll(hash)).to.be.revertedWith(
+                "PollCreator#createPoll: INSUFFICIENT_STAKE"
+            )
+
+            // pendingStake() > 0 && transcoderTotalStake() > 0
+            bondingManagerMock.pendingStake.returns(cost.sub(1))
+            await expect(pollCreator.createPoll(hash)).to.be.revertedWith(
+                "PollCreator#createPoll: INSUFFICIENT_STAKE"
             )
         })
 
         it("creates a poll", async () => {
-            await token.setMockBool(
-                functionSig("transferFrom(address,address,uint256)"),
-                true
-            )
+            const cost = await pollCreator.POLL_CREATION_COST()
+
+            // pendingStake() > POLL_CREATION_COST
+            bondingManagerMock.pendingStake.returns(cost.add(1))
+
             const start = await fixture.rpc.getBlockNumberAsync()
             const end = start + POLL_PERIOD + 1 // + 1 because createPoll tx will mine a new block
-            const tx = await pollCreator.createPoll(hash)
-            const receipt = await tx.wait()
-            await expect(tx.hash)
+
+            let tx = await pollCreator.createPoll(hash)
+            let receipt = await tx.wait()
+            await expect(tx)
                 .to.emit(pollCreator, "PollCreated")
                 .withArgs(receipt.events[0].args[0], hash, end, QUORUM, QUOTA)
+
+            // pendingStake() == POLL_CREATION_COST
+            bondingManagerMock.pendingStake.returns(cost)
+
+            tx = await pollCreator.createPoll(hash)
+            receipt = await tx.wait()
+            await expect(tx)
+                .to.emit(pollCreator, "PollCreated")
+                .withArgs(
+                    receipt.events[0].args[0],
+                    hash,
+                    end + 1,
+                    QUORUM,
+                    QUOTA
+                )
+
+            // transcoderTotalStake() > POLL_CREATION_COST
+            bondingManagerMock.pendingStake.returns(0)
+            bondingManagerMock.transcoderTotalStake.returns(cost.add(1))
+
+            tx = await pollCreator.createPoll(hash)
+            receipt = await tx.wait()
+            await expect(tx)
+                .to.emit(pollCreator, "PollCreated")
+                .withArgs(
+                    receipt.events[0].args[0],
+                    hash,
+                    end + 2,
+                    QUORUM,
+                    QUOTA
+                )
+
+            // transcoderTotalStake() == POLL_CREATION_COST
+            bondingManagerMock.transcoderTotalStake.returns(cost)
+
+            tx = await pollCreator.createPoll(hash)
+            receipt = await tx.wait()
+            await expect(tx)
+                .to.emit(pollCreator, "PollCreated")
+                .withArgs(
+                    receipt.events[0].args[0],
+                    hash,
+                    end + 3,
+                    QUORUM,
+                    QUOTA
+                )
         })
     })
 })
