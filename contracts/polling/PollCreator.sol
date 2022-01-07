88103c113f37b13ec8fc198dc1485aa7ceff5a81
Yondon Fu <yondon.fu@gmail.com>
2022-01-07 08:39:51 -0500

    polling: Use min stake in PollCreator

--- contracts/polling/PollCreator.sol
+++ contracts/polling/PollCreator.sol
@@ -1,7 +1,12 @@
 pragma solidity ^0.5.11;
 
 import "./Poll.sol";
-import "../token/ILivepeerToken.sol";
+
+interface IBondingManager {
+    function transcoderTotalStake(address _addr) external view returns (uint256);
+
+    function pendingStake(address _addr, uint256 _endRound) external view returns (uint256);
+}
 
 contract PollCreator {
     // 33.33%
@@ -12,26 +17,28 @@ contract PollCreator {
     uint256 public constant POLL_PERIOD = 10 * 5760;
     uint256 public constant POLL_CREATION_COST = 100 * 1 ether;
 
-    ILivepeerToken public token;
+    IBondingManager public bondingManager;
 
     event PollCreated(address indexed poll, bytes proposal, uint256 endBlock, uint256 quorum, uint256 quota);
 
-    constructor(address _tokenAddr) public {
-        token = ILivepeerToken(_tokenAddr);
+    constructor(address _bondingManagerAddr) public {
+        bondingManager = IBondingManager(_bondingManagerAddr);
     }
 
     /**
-     * @dev Create a poll by burning POLL_CREATION_COST LPT.
-     *      Reverts if this contract's LPT allowance for the sender < POLL_CREATION_COST.
+     * @notice Create a poll if caller has POLL_CREATION_COST LPT stake (own stake or stake delegated to it).
      * @param _proposal The IPFS multihash for the proposal.
      */
     function createPoll(bytes calldata _proposal) external {
         uint256 endBlock = block.number + POLL_PERIOD;
         Poll poll = new Poll(endBlock);
 
-        require(token.transferFrom(msg.sender, address(this), POLL_CREATION_COST), "LivepeerToken transferFrom failed");
-
-        token.burn(POLL_CREATION_COST);
+        require(
+            // pendingStake() ignores the second arg
+            bondingManager.pendingStake(msg.sender, 0) >= POLL_CREATION_COST ||
+                bondingManager.transcoderTotalStake(msg.sender) >= POLL_CREATION_COST,
+            "PollCreator#createPoll: INSUFFICIENT_STAKE"
+        );
 
         emit PollCreated(address(poll), _proposal, endBlock, QUORUM, QUOTA);
     }
