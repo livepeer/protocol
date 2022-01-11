9c9e317a8e061fcb95a670104383d2e234423cec
Yondon Fu <yondon.fu@gmail.com>
2022-01-11 16:11:55 -0500

    arbitrum: Add DummyGateway for testing

---
+++ contracts/arbitrum/DummyGateway.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.11;
+
+contract DummyGateway {
+    function calculateL2TokenAddress(address _token) external pure returns (address) {
+        return _token;
+    }
+
+    function counterpartGateway() external view returns (address) {
+        return address(this);
+    }
+}
