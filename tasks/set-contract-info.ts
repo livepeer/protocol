a6dde68d6642be40987a905ce79bfe8102bf05d1
Yondon Fu <yondon.fu@gmail.com>
2022-01-11 09:20:41 -0500

    tasks: Init + add unpause & set-contract-info tasks

---
+++ tasks/set-contract-info.ts
@@ -0,0 +1,28 @@
+import {task} from "hardhat/config"
+import {contractId} from "../utils/helpers"
+
+task("set-contract-info", "Set contract info in the Controller")
+    .addParam("name", "Contract name")
+    .addParam("address", "Contract address")
+    .addParam("gitCommitHash", "Git commit hash")
+    .setAction(async (taskArgs, hre) => {
+        const {deployments, ethers} = hre
+        const controllerDeployment = await deployments.get("Controller")
+        const controller = await ethers.getContractAt(
+            "Controller",
+            controllerDeployment.address
+        )
+
+        const id = contractId(taskArgs.name)
+        await controller.setContractInfo(
+            id,
+            taskArgs.address,
+            taskArgs.gitCommitHash
+        )
+
+        const info = await controller.getContractInfo(id)
+
+        console.log(
+            `${taskArgs.name} registered in Controller address=${info[0]} gitCommitHash=${info[1]}`
+        )
+    })
