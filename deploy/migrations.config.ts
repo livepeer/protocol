a60f82b5a86151895a67ee87b996ec6821beeca7
Yondon Fu <yondon.fu@gmail.com>
2022-01-11 08:44:06 -0500

    deploy: Config per network

--- deploy/migrations.config.ts
+++ deploy/migrations.config.ts
@@ -1,6 +1,6 @@
 import {ethers} from "hardhat"
 
-export default {
+const defaultConfig = {
     bondingManager: {
         numTranscoders: 20,
         numActiveTranscoders: 10,
@@ -31,4 +31,44 @@ export default {
             router: "0x70C143928eCfFaf9F5b406f7f4fC28Dc43d68380"
         }
     }
+}
+
+const rinkebyConfig = {
+    bondingManager: {
+        numActiveTranscoders: 100,
+        unbondingPeriod: 2
+    },
+    broker: {
+        unlockPeriod: 100,
+        ticketValidityPeriod: 2
+    },
+    roundsManager: {
+        roundLength: 50,
+        roundLockAmount: 100000
+    },
+    faucet: {
+        requestAmount: ethers.utils.parseEther("10"),
+        requestWait: 1,
+        whitelist: []
+    },
+    minter: {
+        inflation: 137,
+        inflationChange: 3,
+        targetBondingRate: 0
+    }
+}
+
+const arbitrumRinkebyConfig = rinkebyConfig
+
+const networkConfigs: any = {
+    "rinkeby": rinkebyConfig,
+    "arbitrumRinkeby": arbitrumRinkebyConfig
+}
+
+export default function getNetworkConfig(network: string) {
+    if (!(network in networkConfigs)) {
+        return defaultConfig
+    }
+
+    return networkConfigs[network]
 }
