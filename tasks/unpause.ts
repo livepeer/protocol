bc8638eed40586ea16589429843dd9ced25fe62f
Yondon Fu <yondon.fu@gmail.com>
2022-01-11 09:20:41 -0500

    tasks: Init + add unpause & set-contract-info tasks

---
+++ tasks/unpause.ts
@@ -0,0 +1,21 @@
+import {task} from "hardhat/config"
+
+task("unpause", "Unpause the Controller").setAction(async (_, hre) => {
+    const {deployments, ethers} = hre
+    const controllerDeployment = await deployments.get("Controller")
+    const controller = await ethers.getContractAt(
+        "Controller",
+        controllerDeployment.address
+    )
+
+    const paused = await controller.paused()
+    if (!paused) {
+        throw new Error(`Controller ${controller.address} paused=${paused}`)
+    }
+
+    await controller.pause()
+
+    console.log(
+        `Controller ${controller.address} paused=${await controller.paused()}`
+    )
+})
