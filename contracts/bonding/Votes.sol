2ca7b247865f796a9be70b6c65408e7971cc5948
Victor Elias <victorgelias@gmail.com>
2023-05-31 14:46:35 -0300

    contract/votes: Add missing governor extension functions

--- contracts/bonding/Votes.sol
+++ contracts/bonding/Votes.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.9;
 import "./BondingManager.sol";
 import "forge-std/console.sol";
 
-contract Votes {
+abstract contract Votes is Governor {
     uint256 public constant MAX_ROUNDS_WITHOUT_CHECKPOINT = 100;
 
     BondingManager public immutable bondingManager;
@@ -12,6 +12,23 @@ contract Votes {
     constructor(BondingManager _bondingManager) {
         bondingManager = _bondingManager;
     }
+
+    /**
+     * @dev Clock is set to match the current round.
+     */
+    function clock() public view virtual override returns (uint48) {
+        return bondingManager.roundsManager().currentRound();
+    }
+
+    /**
+     * @dev Machine-readable description of the clock as specified in EIP-6372.
+     */
+    // solhint-disable-next-line func-name-mixedcase
+    function CLOCK_MODE() public view virtual override returns (string memory) {
+        return "mode=livepeer_round&from=default";
+    }
+
+    // voting power
 
     function getVotes(address _account, uint256 _timepoint) public view returns (uint256) {
         return _getVotes(_account, _timepoint, "");
@@ -57,5 +74,106 @@ contract Votes {
         }
 
         return 0;
+    }
+
+    // vote counting
+
+    /**
+     * @dev Supported vote types. Matches Governor Bravo ordering.
+     */
+    enum VoteType {
+        Against,
+        For,
+        Abstain
+    }
+
+    struct ProposalVote {
+        uint256 againstVotes;
+        uint256 forVotes;
+        uint256 abstainVotes;
+        mapping(address => bool) hasVoted;
+        mapping(address => VoteType) votes;
+    }
+
+    mapping(uint256 => ProposalVote) private _proposalVotes;
+
+    /**
+     * @dev See {IGovernor-COUNTING_MODE}.
+     */
+    // solhint-disable-next-line func-name-mixedcase
+    function COUNTING_MODE() public pure virtual override returns (string memory) {
+        return "support=bravo&quorum=for,abstain";
+    }
+
+    /**
+     * @dev See {IGovernor-hasVoted}.
+     */
+    function hasVoted(uint256 proposalId, address account) public view virtual override returns (bool) {
+        return _proposalVotes[proposalId].hasVoted[account];
+    }
+
+    /**
+     * @dev Accessor to the internal vote counts.
+     */
+    function proposalVotes(uint256 proposalId)
+        public
+        view
+        virtual
+        returns (
+            uint256 againstVotes,
+            uint256 forVotes,
+            uint256 abstainVotes
+        )
+    {
+        ProposalVote storage proposalVote = _proposalVotes[proposalId];
+        return (proposalVote.againstVotes, proposalVote.forVotes, proposalVote.abstainVotes);
+    }
+
+    /**
+     * @dev See {Governor-_quorumReached}.
+     */
+    function _quorumReached(uint256 proposalId) internal view virtual override returns (bool) {
+        ProposalVote storage proposalVote = _proposalVotes[proposalId];
+
+        return quorum(proposalSnapshot(proposalId)) <= proposalVote.forVotes + proposalVote.abstainVotes;
+    }
+
+    /**
+     * @dev See {Governor-_voteSucceeded}. In this module, the forVotes must be strictly over the againstVotes.
+     */
+    function _voteSucceeded(uint256 proposalId) internal view virtual override returns (bool) {
+        ProposalVote storage proposalVote = _proposalVotes[proposalId];
+
+        return proposalVote.forVotes > proposalVote.againstVotes;
+    }
+
+    /**
+     * @dev See {Governor-_countVote}. In this module, the support follows the `VoteType` enum (from Governor Bravo).
+     */
+    function _countVote(
+        uint256 proposalId,
+        address account,
+        uint8 support,
+        uint256 weight,
+        bytes memory // params
+    ) internal virtual override {
+        ProposalVote storage proposalVote = _proposalVotes[proposalId];
+
+        require(!proposalVote.hasVoted[account], "GovernorVotingSimple: vote already cast");
+        proposalVote.hasVoted[account] = true;
+        proposalVote.votes[account] = support;
+
+        // TODO: need historical delegator state here
+        (, , address delegateAddress, , , ) = bondingManager.getDelegator(accouunt);
+
+        if (support == uint8(VoteType.Against)) {
+            proposalVote.againstVotes += weight;
+        } else if (support == uint8(VoteType.For)) {
+            proposalVote.forVotes += weight;
+        } else if (support == uint8(VoteType.Abstain)) {
+            proposalVote.abstainVotes += weight;
+        } else {
+            revert("GovernorVotingSimple: invalid value for enum VoteType");
+        }
     }
 }
