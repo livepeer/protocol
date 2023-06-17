dd792fb5bd3b8ac27477585255541fcbfe94754a
Victor Elias <victorgelias@gmail.com>
2023-06-17 12:11:11 -0300

    bonding: Stop implementing votes on checckpoints
    Taht will come later with governor. For now we only have the ERC to
    provide a clock, which is common between checkpointing and voting power.

--- contracts/bonding/IBondingCheckpoints.sol
+++ contracts/bonding/IBondingCheckpoints.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
-import "@openzeppelin/contracts-upgradeable/interfaces/IERC5805Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/interfaces/IERC6372Upgradeable.sol";
 
 /**
  * @title Interface for BondingCheckpoints
  */
-interface IBondingCheckpoints is IERC5805Upgradeable {
-    function delegatedAt(address _account, uint256 _timepoint) external view returns (address);
+interface IBondingCheckpoints is IERC6372Upgradeable {
+    // BondingManager hooks
 
     function checkpointBonding(
         address _account,
@@ -22,4 +22,12 @@ interface IBondingCheckpoints is IERC5805Upgradeable {
     function checkpointTotalActiveStake(uint256 _totalStake, uint256 _round) external;
 
     function hasCheckpoint(address _account) external returns (bool);
+
+    // Historical stake access functions
+
+    function getTotalActiveStakeAt(uint256 _round) external view returns (uint256);
+
+    function getAccountActiveStakeAt(address _account, uint256 _round) external view returns (uint256);
+
+    function getDelegateAddressAt(address _account, uint256 _round) external view returns (address);
 }
