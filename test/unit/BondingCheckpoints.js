c4b8e764c0c853659b892357d5e579015391961f
Victor Elias <victorgelias@gmail.com>
2023-06-19 20:31:47 -0300

    test/bonding: Add initialization tests

--- test/unit/BondingCheckpoints.js
+++ test/unit/BondingCheckpoints.js
@@ -7,6 +7,7 @@ import {solidity} from "ethereum-waffle"
 import {constants} from "ethers"
 
 chai.use(solidity)
+const {expect} = chai
 
 // TODO: to be moved in a separate util/config (i.e: chai-setup)
 chai.use(function(chai) {
@@ -80,6 +81,146 @@ describe("BondingCheckpoints", () => {
 
     afterEach(async () => {
         await fixture.tearDown()
+    })
+
+    describe("init bonding checkpoint", () => {
+        let transcoder
+        let delegator
+        let currentRound
+
+        const setRound = async round => {
+            await fixture.roundsManager.setMockUint256(
+                functionSig("currentRound()"),
+                round
+            )
+            await fixture.roundsManager.execute(
+                bondingManager.address,
+                functionSig("setCurrentRoundTotalActiveStake()")
+            )
+        }
+
+        beforeEach(async () => {
+            transcoder = signers[0]
+            delegator = signers[1]
+            currentRound = 100
+
+            // Unregister BondingCheckpoints from controller so it doesn't get state updates. This is the trick we use
+            // to simulate not having the checkpoints deployed yet to see the migration of the data once it is
+            await fixture.register("BondingCheckpoints", constants.AddressZero)
+
+            await fixture.roundsManager.setMockBool(
+                functionSig("currentRoundInitialized()"),
+                true
+            )
+            await fixture.roundsManager.setMockBool(
+                functionSig("currentRoundLocked()"),
+                false
+            )
+
+            // Round R-2
+            await setRound(currentRound - 2)
+
+            await bondingManager
+                .connect(transcoder)
+                .bond(1000, transcoder.address)
+            await bondingManager
+                .connect(transcoder)
+                .transcoder(50 * PERC_MULTIPLIER, 25 * PERC_MULTIPLIER)
+
+            // Round R-1
+            await setRound(currentRound - 1)
+
+            await bondingManager
+                .connect(delegator)
+                .bond(1000, transcoder.address)
+
+            // Round R
+            await setRound(currentRound)
+
+            await fixture.minter.setMockUint256(
+                functionSig("createReward(uint256,uint256)"),
+                1000
+            )
+
+            // Now let's register back the BondingCheckpoints contract as if it was deployed in the current round
+            await fixture.register(
+                "BondingCheckpoints",
+                bondingCheckpoints.address
+            )
+        })
+
+        const stakeAt = (account, round) =>
+            bondingCheckpoints
+                .getAccountActiveStakeAt(account.address, round)
+                .then(n => n.toString())
+
+        it("should revert if bonding checkpoint is not registered", async () => {
+            await fixture.register("BondingCheckpoints", constants.AddressZero)
+
+            await expect(
+                bondingManager.initBondingCheckpoint(transcoder.address)
+            ).to.be.revertedWith("bonding checkpoints not available")
+        })
+
+        it("should revert if account already initialized", async () => {
+            await bondingManager.initBondingCheckpoint(transcoder.address)
+
+            await expect(
+                bondingManager.initBondingCheckpoint(transcoder.address)
+            ).to.be.revertedWith("account already checkpointed")
+        })
+
+        it("should allow querying on the round after it is called", async () => {
+            await expect(stakeAt(transcoder, currentRound)).to.be.revertedWith(
+                "findLowerBound: empty array"
+            )
+
+            await bondingManager.initBondingCheckpoint(transcoder.address)
+
+            // Round R+1
+            await setRound(currentRound + 1)
+
+            // checkpoint only valid for next round
+            await expect(stakeAt(transcoder, currentRound)).to.be.revertedWith(
+                "findLowerBound: all values in array are higher than searched value"
+            )
+
+            assert.equal(await stakeAt(transcoder, currentRound + 1), 2000)
+        })
+
+        it("should have no problems if state gets updated again in round", async () => {
+            await bondingManager.initBondingCheckpoint(transcoder.address)
+
+            await bondingManager.connect(transcoder).reward()
+
+            // Round R+1
+            await setRound(currentRound + 1)
+
+            assert.equal(await stakeAt(transcoder, currentRound + 1), 3000)
+        })
+
+        it("should still create checkpoints even if never inited", async () => {
+            await bondingManager.connect(transcoder).reward()
+
+            // Round R+1
+            await setRound(currentRound + 1)
+
+            assert.equal(await stakeAt(transcoder, currentRound + 1), 3000)
+
+            await expect(
+                stakeAt(delegator, currentRound + 1)
+            ).to.be.revertedWith("findLowerBound: empty array")
+
+            await bondingManager
+                .connect(delegator)
+                .bond(1000, transcoder.address)
+
+            // Round R+2
+            await setRound(currentRound + 2)
+
+            assert.equal(await stakeAt(delegator, currentRound + 2), 2250) // 1000 + 500 * 1000 / 2000 + 1000
+            assert.equal(await stakeAt(transcoder, currentRound + 2), 4000)
+        })
     })
 
     describe("scenarios", () => {
@@ -929,10 +1070,5 @@ describe("BondingCheckpoints", () => {
     })
 
     // TODO: more tests:
-    // - migration tests:
-    //   - everything still works if we don't call `initBondingCheckpoint`
-    //   - when we do call it, checkpoints should work starting on the next round
-    //   - make sure it still works even if we init before a transcoder has called reward()
-    //   - if we don't call it, it's a known issue that we will only have state starting on the next update made
     // - sorted array tests (separate file)
 })
