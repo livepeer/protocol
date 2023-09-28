296085662660bbe32a252b29f78c0f37f79fbf1d
Victor Elias <victorgelias@gmail.com>
2023-09-27 22:46:58 -0400

    [devnet-compat] treasury: Invert order of quota field
    The LivepeerGovernor deployed to the devnet had the
    _proposalTallies and quota fields on a different order.
    To be able to upgrade these have to be inverted but that
    should not affect any of the functionality.

--- contracts/treasury/GovernorCountingOverridable.sol
+++ contracts/treasury/GovernorCountingOverridable.sol
@@ -53,14 +53,14 @@ abstract contract GovernorCountingOverridable is Initializable, GovernorUpgradea
         mapping(address => ProposalVoterState) voters;
     }
 
-    // Maps proposal IDs to their corresponding vote tallies.
-    mapping(uint256 => ProposalTally) private _proposalTallies;
-
     /**
      * @notice The required percentage of "for" votes in relation to the total opinionated votes (for and against) for
      * a proposal to succeed. Represented as a MathUtils percentage value (e.g. 6 decimal places).
      */
     uint256 public quota;
+
+    // Maps proposal IDs to their corresponding vote tallies.
+    mapping(uint256 => ProposalTally) private _proposalTallies;
 
     function __GovernorCountingOverridable_init(uint256 _quota) internal onlyInitializing {
         __GovernorCountingOverridable_init_unchained(_quota);
